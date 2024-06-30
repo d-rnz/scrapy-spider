431c283d6ac6dc3e5a1cebc39116dff2648a00c1
d-rnz <157235656+d-rnz@users.noreply.github.com>
2024-06-30 14:15:42 +0800

    Implement parse function for spider

--- webscraper/webscraper/spiders/webspider.py
+++ webscraper/webscraper/spiders/webspider.py
@@ -7,4 +7,17 @@ class WebspiderSpider(scrapy.Spider):
     start_urls = ["https://asuracomic.net"]
 
     def parse(self, response):
-        pass
+        mangas = response.css("div.utao.styletwo")
+
+        for manga in mangas:
+            yield {
+                "image": manga.css("img::attr(src)").get(),
+                "title": manga.css("h4::text").get(),
+                "chapter": manga.css("ul.Manhwa li a::text").get(),
+                "link": manga.css("a.series::attr(href)").get(),
+            }
+
+        next_page = response.css("a.r::attr(href)").get()
+
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
